315bc10fcf44900590f8b9921d52ae375ee1af4d
Jamison Cozart <jamisoncozart@gmail.com>
2020-01-20 11:16:14 -0800

    Add and remove active class for nav footer based on clicked link

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,30 @@
-//create current-page variable
+var currentPage = 1;
+
+$(document).ready(function() {
+  $("#homeLink").click(function() {
+    $("#homeLink").addClass("navActive");
+    $("#projectsLink").removeClass();
+    $("#aboutLink").removeClass();
+    $("#contactLink").removeClass();
+  })
+  $("#projectsLink").click(function() {
+    $("#projectsLink").addClass("navActive");
+    $("#homeLink").removeClass();
+    $("#aboutLink").removeClass();
+    $("#contactLink").removeClass();
+  });
+  $("#aboutLink").click(function() {
+    $("#aboutLink").addClass("navActive");
+    $("#homeLink").removeClass();
+    $("#projectsLink").removeClass();
+    $("#contactLink").removeClass();
+  });
+  $("#contactLink").click(function() {
+    $("#contactLink").addClass("navActive");
+    $("#homeLink").removeClass();
+    $("#aboutLink").removeClass();
+    $("#projectsLink").removeClass();
+  })
+})
+
+  //create current-page variable
