20072a86a521950cbb38d99e666f6e0b3c4ad135
Jamison Cozart <jamisoncozart@gmail.com>
2020-01-20 15:29:21 -0800

    Add scrollToTop() function for all nav buttons

--- js/scripts.js
+++ js/scripts.js
@@ -14,6 +14,7 @@ $(document).ready(function() {
     $("#contact").hide();
     $("#home").show();  
     currentPage = 1;
+    scrollToTop();
   })
   $("#projectsLink").click(function(event) {
     event.preventDefault();
@@ -28,6 +29,7 @@ $(document).ready(function() {
     $("#contact").hide();
     $("#projects").show();
     currentPage = 2;
+    scrollToTop();
   });
   $("#aboutLink").click(function(event) {
     event.preventDefault();
@@ -42,6 +44,7 @@ $(document).ready(function() {
     $("#contact").hide();
     $("#about").show();
     currentPage = 3;
+    scrollToTop();
   });
   $("#contactLink").click(function(event) {
     event.preventDefault();
@@ -56,9 +59,18 @@ $(document).ready(function() {
     $("#projects").hide();
     $("#contact").show();
     currentPage = 4;
+    scrollToTop();
   })
   $("#form").submit(function(event) {
     event.preventDefault();
     alert("Form submission is currently unavailable. Please contact me in another way. Thank you!");
   })
 })
+
+function scrollToTop() {
+  var position = document.documentElement.scrollTop || document.body.scrollTop;
+  if(position > 0) {
+    window.requestAnimationFrame(scrollToTop);
+    window.scrollTo(0, position - position / 8);
+  }
+};
