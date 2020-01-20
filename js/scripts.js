ffcef0e1b4ab066e514581775250cb291f5da239
Jamison Cozart <jamisoncozart@gmail.com>
2020-01-20 11:21:27 -0800

    Hide/show page sections based on selected nav link in footer

--- js/scripts.js
+++ js/scripts.js
@@ -2,28 +2,52 @@ var currentPage = 1;
 
 $(document).ready(function() {
   $("#homeLink").click(function() {
+    //add active/remove active class
     $("#homeLink").addClass("navActive");
     $("#projectsLink").removeClass();
     $("#aboutLink").removeClass();
     $("#contactLink").removeClass();
+    //hide/show selected page
+    $("#projects").hide();
+    $("#about").hide();
+    $("#contact").hide();
+    $("#home").show();  
   })
   $("#projectsLink").click(function() {
+    //add active/remove active class
     $("#projectsLink").addClass("navActive");
     $("#homeLink").removeClass();
     $("#aboutLink").removeClass();
     $("#contactLink").removeClass();
+    //hide/show selected page
+    $("#home").hide();
+    $("#about").hide();
+    $("#contact").hide();
+    $("#projects").show();
   });
   $("#aboutLink").click(function() {
+    //add active/remove active class
     $("#aboutLink").addClass("navActive");
     $("#homeLink").removeClass();
     $("#projectsLink").removeClass();
     $("#contactLink").removeClass();
+    //hide/show selected page
+    $("#home").hide();
+    $("#projects").hide();
+    $("#contact").hide();
+    $("#about").show();
   });
   $("#contactLink").click(function() {
+    //add active/remove active class
     $("#contactLink").addClass("navActive");
     $("#homeLink").removeClass();
     $("#aboutLink").removeClass();
     $("#projectsLink").removeClass();
+    //hide/show selected page
+    $("#home").hide();
+    $("#about").hide();
+    $("#projects").hide();
+    $("#contact").show();
   })
 })
 
